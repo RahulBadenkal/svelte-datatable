9e8d994319efbd471002c970526c93cccb43ac18
Rahul <rahulbadenkal@gmail.com>
2022-02-04 23:35:44 +0530

    Common

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,12 +1,5 @@
 <!-- Typescript -->
 <script lang="ts">
-import Table from "../components/Table.svelte";
-import Tbody from "../components/Tbody.svelte";
-import Td from "../components/Td.svelte";
-import Th from "../components/Th.svelte";
-import Thead from "../components/Thead.svelte";
-import Tr from "../components/Tr.svelte";
-
 
   /* Components */
   
@@ -17,32 +10,30 @@ import Tr from "../components/Tr.svelte";
 
 <!-- Template -->
 <div class="h-screen flex justify-center items-center">
-    <Table>
-      <Thead>
-        <Th>Column1</Th>
-        <Th>Column2</Th>
-        <Th>Column3</Th>
-      </Thead>
-      <Tbody>
-        <Tr>
-          <Td>
-            <div class="hello">Cell (1, 1)</div>
-          </Td>
-          <Td>Cell (1, 2)</Td>
-          <Td>Cell (1, 3)</Td>
-        </Tr>
-        <Tr>
-          <Td>Cell (2, 1)</Td>
-          <Td>Cell (2, 2)</Td>
-          <Td>Cell (2, 3)</Td>
-        </Tr>
-        <Tr>
-          <Td>Cell (3, 1)</Td>
-          <Td>Cell (3, 2)</Td>
-          <Td>Cell (3, 3)</Td>
-        </Tr>
-      </Tbody>
-    </Table>
+  <table>
+    <thead>
+      <th>Column1</th>
+      <th>Column2</th>
+      <th>Column3</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Cell (1, 1)</td>
+        <td>Cell (1, 2)</td>
+        <td>Cell (1, 3)</td>
+      </tr>
+      <tr>
+        <td>Cell (2, 1)</td>
+        <td>Cell (2, 2)</td>
+        <td>Cell (2, 3)</td>
+      </tr>
+      <tr>
+        <td>Cell (3, 1)</td>
+        <td>Cell (3, 2)</td>
+        <td>Cell (3, 3)</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 
 
